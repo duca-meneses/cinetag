568f52924ff04f1c1ffb8cfc8f1eab023b853f0b
Carlos Eduardo <ducameneses16@gmail.com>
2023-08-05 00:52:21 -0300

    feat: :sparkles: Atualizando o index.js

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,24 @@
+import Banner from "components/Banner";
+import styles from "./Favorites.module.css";
+import Title from "components/Title";
+import Card from "components/Card";
+import { useFavoriteContext } from "Context/Favorites";
+
+function Favorites() {
+  const { favorite } = useFavoriteContext();
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Title>
+        <h1> Meus Favoritos</h1>
+      </Title>
+      <section className={styles.container}>
+        {favorite.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </section>
+    </>
+  );
+}
+
+export default Favorites;
