9ec85fee1e0fb83de5a621f279aa1409f6eef9ea
Carlos Eduardo <ducameneses16@gmail.com>
2023-08-05 02:06:22 -0300

    feat: :sparkles: Modificando o componente Card para conectar com a rota do Player

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,8 @@
 import { useFavoriteContext } from "Context/Favorites";
 import styles from "./Card.module.css";
 import iconFavorite from "./favorite_outline.png";
-import iconDisfavor from "./favorite.png"
+import iconDisfavor from "./favorite.png";
+import { Link } from "react-router-dom";
 
 function Card({ id, titulo, capa }) {
   const { favorite, addFavorite } = useFavoriteContext();
@@ -9,8 +10,10 @@ function Card({ id, titulo, capa }) {
   const icon = !isFavorite ? iconFavorite : iconDisfavor;
   return (
     <div className={styles.container}>
-      <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
+      <Link className={styles.link} to={`/${id}`}>
+        <img src={capa} alt={titulo} className={styles.capa} />
+        <h2>{titulo}</h2>
+      </Link>
       <img
         src={icon}
         alt="Favoritar Filme"
